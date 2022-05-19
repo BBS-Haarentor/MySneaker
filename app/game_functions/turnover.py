b5ae4a06703c6439199d43035b203a563a4aec44
JakobGrimm <94485903+JakobGrimm@users.noreply.github.com>
2022-05-19 10:46:05 +0200

    RechnungenFürLerer

--- app/game_functions/turnover.py
+++ app/game_functions/turnover.py
@@ -1,7 +1,7 @@
 
 # Market_Planned_quantity, Market_Price_Pro_Unit_Offer, Tender_Planned_quantity, Tender_Price_Pro_Unit_Offer, Research_and_development_costs, Plan_Publicity_expenditure
-# Workers, Next_Period_Workers
-#
+# Workers, Next_Period_Workers, Consern_Quantity_offered, Consern_advertisement_payed, Consern_share_of_adventisement, Demand_in_the_market, sale_through_advertising_
+# Werbung_Bezalt
 #
 
 #def turnover_simple(value_dict: dict) -> dict:
@@ -39,35 +39,38 @@
     #FertigeSneakerLagerKosten_Plan =
 
 #mySneaker_Spieledatein Daten für Lehrkraft
-def turnover_simple(value_dict: dict) -> dict:
-    StückzahlMarkt = dict["Market_Planned_quantity"]            #Vertrieb und Absatz!D19
-    PreisMarkt = dict["Market_Price_Pro_Unit_Offer"]            #Vertrieb und Absatz!E19
-    StückzahlAusschreibung = dict["Tender_Planned_quantity"]    #Vertrieb und Absatz!D20
-    PreisAusschreibung = dict["Tender_Price_Pro_Unit_Offer"]    #Vertrieb und Absatz!E20
+def turnover_simple() -> int:
+UnternehmenAnzahl = 
+#Sortieren nach Preise(nitrigster zu erst).
+for i = 0 to UnternehmenAnzahl do:
+    def turnover_simple(value_dict: dict) -> dict:
+        StückzahlMarkt = dict["Market_Planned_quantity"]            #Vertrieb und Absatz!D19
+        PreisMarkt = dict["Market_Price_Pro_Unit_Offer"]            #Vertrieb und Absatz!E19
+        StückzahlAusschreibung = dict["Tender_Planned_quantity"]    #Vertrieb und Absatz!D20
+        PreisAusschreibung = dict["Tender_Price_Pro_Unit_Offer"]    #Vertrieb und Absatz!E20
 
-    Rationalisierung = dict["Research_and_development_costs"]   #'Marketing  F&E'!E12
-    Werbung = dict["Plan_Publicity_expenditure"]                #Statistik & Finanzen'!D42
+        Rationalisierung = dict["Research_and_development_costs"]   #'Marketing  F&E'!E12
+        Werbung = dict["Plan_Publicity_expenditure"]                #Statistik & Finanzen'!D42
 
-    lAenderungenderMA =  - dict["Workers"]                      #Personal!D18-Personal!D10
+        lAenderungenderMA =  - dict["Workers"]                      #Personal!D18-Personal!D10
 
-    Darlehenstand = dict["Next_Period_Workers"]                 #Statistik & Finanzen'!E30
+        Darlehenstand = dict["Next_Period_Workers"]                 #Statistik & Finanzen'!E30
 
 
-def turnover_simple(value_dict : dict) ->dict:
-    
-    UnternehmenGeboteneStückzahl = dict["Consern_Quantity_offered"]
-    UnternehmenPreise = dict["Consern_price"]
+    def turnover_simple(value_dict : dict) ->dict:
+        
+        UnternehmenGeboteneStückzahl = dict["Consern_Quantity_offered"]
+        UnternehmenPreise = dict["Consern_price"]
 
-def turnover_simple(value_dict : dict) ->dict:
-    UnternehmenGebotenWerbung = dict["Consern_advertisement_payed"]
-    UnternehmenWerbeanteil = dict["Consern_share_of_adventisement"]
+    def turnover_simple(value_dict : dict) ->dict:
+        UnternehmenGebotenWerbung = dict["Consern_advertisement_payed"]
+        UnternehmenWerbeanteil = dict["Consern_share_of_adventisement"]
 
-    #NachfrageAufdemMarkt = dict["Demand_in_the_market"]
-    #VerkaufDurchWerbungMax = NachfrageAufdemMarkt / 100 * Dict["sale_through_advertising"]
-    #if UnternehmenGeboteneStückzahl < 0 
+        NachfrageAufdemMarkt = dict["Demand_in_the_market"]
+        VerkaufDurchWerbungMax = NachfrageAufdemMarkt / 100 * Dict["sale_through_advertising_%"]
+        Werbeanteil = round(100/VerkaufDurchWerbungMax*dict["Werbung_Bezalt"])
+        VerkaufDurchWerbung = round(VerkaufDurchWerbungMax/100*Werbeanteil, 0)
 
-    #VerkaufDurchWerbung = 
-
-#def turnover_simple(value_dict : dict) ->dict:
-#    UnternehmenVerkaufOhneWerbung = 
+    def turnover_simple(value_dict : dict) ->dict:
+        UnternehmenVerkaufOhneWerbung = 
  
