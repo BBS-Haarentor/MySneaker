75521d9168fcc73d83b70fa0b2b82b55c5e59830
JakobGrimm <94485903+JakobGrimm@users.noreply.github.com>
2022-05-29 12:24:43 +0200

    Rechnugnen

--- app/game_functions/turnover.py
+++ app/game_functions/turnover.py
@@ -10,54 +10,50 @@ UnternehmenAnzahl = dict["UnternehmenAnzahl"]
 #Sortieren nach Preise(nitrigster zu erst).
 #anzahl der id durchzählen
 
-def turnover_simple(value_dict: dict) -> dict:
+def turnover_Rechnungen(value_dict: dict) -> dict:
     NachfrageAufdemMarkt = dict["Demand_in_the_market"]
     VerkaufDurchWerbungMax = NachfrageAufdemMarkt / 100 * Dict["sale_through_advertising_%"]
-    NachfrageAufdemMarkt = NachfrageAufdemMarkt - VerkaufDurchWerbungMax
+    NachfrageAufdemMarkt = NachfrageAufdemMarkt - VerkaufDurchWerbungMax  
 
-for i = 1 in UnternehmenAnzahl:
-    def turnover_simple(value_dict: dict) -> dict:
-        StückzahlMarkt = dict["Market_Planned_quantity"]                     #Vertrieb und Absatz!D19
-        PreisMarkt = dict["Market_Price_Pro_Unit_Offer"]                     #Vertrieb und Absatz!E19
-        StückzahlAusschreibung = dict["Tender_Planned_quantity"]             #Vertrieb und Absatz!D20
-        PreisAusschreibung = dict["Tender_Price_Pro_Unit_Offer"]             #Vertrieb und Absatz!E20
+#for i = 1 in UnternehmenAnzahl:
+    #
+    StückzahlMarkt = dict["Market_Planned_quantity"]                     #Vertrieb und Absatz!D19
+    PreisMarkt = dict["Market_Price_Pro_Unit_Offer"]                     #Vertrieb und Absatz!E19
+    StückzahlAusschreibung = dict["Tender_Planned_quantity"]             #Vertrieb und Absatz!D20
+    PreisAusschreibung = dict["Tender_Price_Pro_Unit_Offer"]             #Vertrieb und Absatz!E20
 
-        Rationalisierung = dict["Research_and_development_costs"]            #'Marketing  F&E'!E12
-        Werbung = dict["Plan_Publicity_expenditure"]                         #Statistik & Finanzen'!D42
+    Rationalisierung = dict["Research_and_development_costs"]            #'Marketing  F&E'!E12
+    Werbung = dict["Plan_Publicity_expenditure"]                         #Statistik & Finanzen'!D42
 
-        lAenderungenderMA =  - dict["Workers"]                               #Personal!D18-Personal!D10         bis heir weiter gegeben
+    lAenderungenderMA =  - dict["Workers"]                               #Personal!D18-Personal!D10         bis heir weiter gegeben
 
-        Darlehenstand = dict["Next_Period_Workers"]                          #Statistik & Finanzen'!E30
+    Darlehenstand = dict["Next_Period_Workers"]                          #Statistik & Finanzen'!E30
+    #Verkauf
+    UnternehmenGeboteneStückzahl = dict["Consern_Quantity_offered"]
+    UnternehmenPreise = dict["Consern_price"]
+    #werbung
+    UnternehmenGebotenWerbung = dict["Werbung_Bezalt"]
 
-    def turnover_simple(value_dict : dict) ->dict:
-        
-        UnternehmenGeboteneStückzahl = dict["Consern_Quantity_offered"]
-        UnternehmenPreise = dict["Consern_price"]
+    Werbeanteil = round(100/VerkaufDurchWerbungMax*dict["Werbung_Bezalt"])
+    VerkaufDurchWerbung = round(VerkaufDurchWerbungMax/100*Werbeanteil, 0)
+    StückzahlNachWerbeverkauf = UnternehmenGeboteneStückzahl - VerkaufDurchWerbung
+    #Verkauf der Firma
+    if NachfrageAufdemMarkt < VerkaufDurchWerbung
+        UnternehmenVerkaufOhneWerbung = NachfrageAufdemMarkt
+    else
+        UnternehmenVerkaufOhneWerbung = StückzahlNachWerbeverkauf
+    GesamterVerkauf = UnternehmenVerkaufOhneWerbung + VerkaufDurchWerbung
+    NachfrageAufdemMarkt = NachfrageAufdemMarkt - UnternehmenVerkaufOhneWerbung
+    Umsatz = UnternehmenPreise * GesamterVerkauf
 
-    def turnover_simple(value_dict : dict) ->dict:
-        UnternehmenGebotenWerbung = dict["Werbung_Bezalt"]
-
-        Werbeanteil = round(100/VerkaufDurchWerbungMax*dict["Werbung_Bezalt"])
-        VerkaufDurchWerbung = round(VerkaufDurchWerbungMax/100*Werbeanteil, 0)
-        StückzahlNachWerbeverkauf = UnternehmenGeboteneStückzahl - VerkaufDurchWerbung
-
-    def turnover_simple(value_dict : dict) ->dict:
-        if NachfrageAufdemMarkt < VerkaufDurchWerbung
-            UnternehmenVerkaufOhneWerbung = NachfrageAufdemMarkt
-        else
-            UnternehmenVerkaufOhneWerbung = StückzahlNachWerbeverkauf
-        GesamterVerkauf = UnternehmenVerkaufOhneWerbung + VerkaufDurchWerbung
-        NachfrageAufdemMarkt = NachfrageAufdemMarkt - UnternehmenVerkaufOhneWerbung
-        Umsatz = UnternehmenPreise * GesamterVerkauf
-
-    def turnover_simple(value_dict : dict) -> dict:
-        Buget(Kumuliert) = dict["Paid_Entwickelung"]                                            #int SpilereEingabe
-        if Buget(Kumuliert) >= 2500
-            StuffeEinsEntwickelung = true                                                       #Warhetiswert
-        if Buget(Kumuliert) >= 5000
-            StuffeZweiEntwickelung = true
-        if Buget(Kumuliert) >= 7500
-            StuffeDreiEntwickelung = true
-        if Buget(Kumuliert) >= 10000
-            StuffeFierEntwickelung = true
-        if Buget(Kumuliert) >= 12500
+    #Entwickelung
+    Buget(Kumuliert) = dict["Paid_Entwickelung"]                                            #int SpilereEingabe
+    if Buget(Kumuliert) >= 2500
+        StuffeEinsEntwickelung = true                                                       #Warhetiswert
+    if Buget(Kumuliert) >= 5000
+        StuffeZweiEntwickelung = true
+    if Buget(Kumuliert) >= 7500
+        StuffeDreiEntwickelung = true
+    if Buget(Kumuliert) >= 10000
+        StuffeFierEntwickelung = true
+    if Buget(Kumuliert) >= 12500
