fca21d35ef2355b01480667a4508a5c074071123
Optischa <fabian@eversit.de>
2022-06-01 22:41:20 +0200

    Save via Tom

--- mysneaker/src/components/DashBoardPage.js
+++ mysneaker/src/components/DashBoardPage.js
@@ -2,6 +2,7 @@
 import SideNavBar from "./SideNavBar"
 import { useState, useEffect, useRef } from "react";
 import LoginPage from "./LoginPage";
+import LehrerPage from "./LehrerPage";
 import Container from "./Container";
 import Cookies from "js-cookie";
 
@@ -13,7 +14,7 @@ const DashBoardPage = () => {
   const ProductionRef = useRef(null);
   const AbsatzRef = useRef(null);
 
-  const [state, setState] = useState((Cookies.get("session") === undefined ? "Login" : "Lager/Beschaffung"))
+  const [state, setState] = useState((Cookies.get("session") === undefined || Cookies.get("session") === "" ? "Login" : "Lager/Beschaffung"))
 
   const OnClick = (text) => {
     console.log(text)
@@ -33,15 +34,16 @@ const DashBoardPage = () => {
     }else if(text === "Logout"){
       window.location.href = "/logout"
     }
-      
-
+  
   }
+
+   var iflehe = true;
 
   return (
     <div className="h-screen w-screen bg-[#f7fafc] flex" >
       <SideNavBar OnClick={OnClick} state={state} />
 
-      {state == "Login" ? <LoginPage /> : <Container MarketingRef={MarketingRef} FinanzenRef={FinanzenRef} AbsatzRef={AbsatzRef} LagerBeschaffungRef={LagerBeschaffungRef} ProductionRef={ProductionRef} PersonalRef={PersonalRef}/>}
+      {state == "Login" ? <LoginPage /> : ( iflehe ? <LehrerPage/>  : <Container MarketingRef={MarketingRef} FinanzenRef={FinanzenRef} AbsatzRef={AbsatzRef} LagerBeschaffungRef={LagerBeschaffungRef} ProductionRef={ProductionRef} PersonalRef={PersonalRef}/>)}
     </div>
   )
 }
