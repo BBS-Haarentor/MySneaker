c0d49c50620ad5867bbb086c6a07ad29a7ecfb32
Jost Atzler <jost.atzler@btc-ag.com>
2022-06-28 21:29:56 +0200

    fixed default values in stock

--- app/models/stock.py
+++ app/models/stock.py
@@ -17,11 +17,11 @@ class Stock(StockBase, table=True):
     finished_sneaker_count: int = Field(default=0)#
     employees_count: int = Field(default=8)#
     research_budget: float = Field(default=0.0)#
-    account_balance: float = Field(default=5000.00)#
+    account_balance: float = Field(default=50000.00)#
     credit_taken: float  = Field(default=0.00)#
     real_sales: int = Field(default=0)#
     income_from_sales: float = Field(default=0.00)#
-    research_production_modifier: float = Field(default=0.00)#
+    research_production_modifier: float = Field(default=1.00)#
 
     machine_1_space: int = Field(default=1)
     machine_2_space: int = Field(default=0)
