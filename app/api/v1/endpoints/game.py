73f771922e72098e0c6539bfb7f7da3c92142468
Jost Atzler <jost.atzler@btc-ag.com>
2022-06-28 11:26:35 +0200

    added missing auth decorator

--- app/api/v1/endpoints/game.py
+++ app/api/v1/endpoints/game.py
@@ -142,6 +142,7 @@ async def get_cycle_index(game_id: int, current_user: User = Depends(get_current
 
 
 @router.put("/edit", status_code=status.HTTP_202_ACCEPTED, response_model=GameResponse)
+@teacher_auth_required
 async def game_patch(game_patch: GamePatch, session: AsyncSession = Depends(get_async_session)) -> GameResponse | None:
     updated_game: Game | None = await edit_game(patch_data=game_patch, session=session)
     if isinstance(updated_game, NoneType):
