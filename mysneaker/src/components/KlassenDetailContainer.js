f046a00224dd915b41a1f597dd469137826632be
Optischa <fabian.evers2602@gmail.com>
2022-08-18 23:03:35 +0200

    Beginning of API module

--- mysneaker/src/components/KlassenDetailContainer.js
+++ mysneaker/src/components/KlassenDetailContainer.js
@@ -522,30 +522,6 @@ const KlassenDetailContainer = ({ userId, cycle_index }) => {
                 )
             }
         })
-        /*setModalConfirm(
-            <>
-                <div className={"block"}>
-                    <div
-                        className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
-                        id="my-modal"
-                    ></div>
-                    <div
-                        className="fixed text-gray-600 flex items-center justify-center overflow-auto z-50 bg-black bg-opacity-40 left-0 right-0 top-0 bottom-0">
-                        <div
-                            className="text-center bg-white rounded-xl shadow-2xl p-6 sm:w-8/12 mx-10 ">
-
-                            <div className="hero container max-w-screen-lg mx-auto p-5">
-                                <h1>Möchtes du wirklich die Maschine {name} für {formatter.format(price)} Kaufen?</h1>
-                                <div className='pt-4'>
-                                <button onClick={() => doMagicToBuyMachine(name, price)} className="px-4 py-2 text-sm bg-green-500  rounded-xl border transition-colors duration-150 ease-linear border-gray-200 text-gray-500 focus:outline-none focus:ring-0 font-bold hover:bg-green-700 focus:bg-green-600 focus:text-indigo">Ja</button>
-                                <button onClick={() => setModalConfirm(<></>)} className="px-4 py-2  text-sm bg-red-500 rounded-xl border transition-colors duration-150 ease-linear border-gray-200 text-gray-500 focus:outline-none focus:ring-0 font-bold hover:bg-red-700 focus:bg-red-600 focus:text-indigo">Nein</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </>
-        )*/
     }
     const doMagicToBuyMachine = (name, preis, id) => {
 
