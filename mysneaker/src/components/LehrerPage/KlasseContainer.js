77622f548ea18a1d8d0e2b19cf3fa3b42740702a
Sabberkopp <Sabberteam@gmail.com>
2022-08-21 15:16:12 +0200

    Co-authored-by: Optischa <Optischa@users.noreply.github.com>

--- mysneaker/src/components/LehrerPage/KlasseContainer.js
+++ mysneaker/src/components/LehrerPage/KlasseContainer.js
@@ -33,6 +33,63 @@ const KlasseContainer = ({updateCompany, select, setSelect, companyId, current_c
         })
     }, [])
 
+    const deleteGame = () =>{
+        console.log("deleteGame")
+        Swal.fire({
+            title: 'Spiel Löschen?',
+            text: "Wollen Sie wirklich das Spiel Löschen?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#7FFFD4',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ja, Löschen',
+            cancelButtonText: "Nein"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                    var myHeaders = new Headers();
+                    myHeaders.append("Content-Type", "application/json");
+                    myHeaders.append("Authorization", "Bearer " + Cookies.get("session"))
+                    myHeaders.append('Access-Control-Allow-Origin', '*')
+    
+                    var requestOptions1 = {
+                        method: 'PUT',
+                        headers: myHeaders
+                    };
+    
+                    fetch(process.env.REACT_APP_MY_API_URL + '/api/v1/game/toggle_active/' + gameId, requestOptions1).then((element) => {
+                        if (element.status === 202) {
+                    
+                        }
+                    }).then(() => {
+                        var requestOptions = {
+                            method: 'DELETE',
+                            headers: myHeaders
+                        };
+    
+                        fetch(process.env.REACT_APP_MY_API_URL + '/api/v1/game/delete/' + gameId, requestOptions).then((element) => {
+                            if (element.status === 200) {
+                                //TODO rederect
+                                Swal.fire(
+                                    'Spiel Gelöscht!',
+                                    'Sie haben das Spiel Erfolgreich gelöscht',
+                                    'success'
+                                )
+                            } else {
+                                element.json().then((element1) => {
+                                    Swal.fire(
+                                        'Fehler!',
+                                        "element1",
+                                        'error'
+                                    )
+                                })
+                            }
+                        })
+                    });
+            }
+        });
+
+    }
+
     const deleteUser = (companyId) => {
         Swal.fire({
             title: 'Benutzer Löschen?',
@@ -244,8 +301,8 @@ const KlasseContainer = ({updateCompany, select, setSelect, companyId, current_c
                                         )
                                     })}
                                 </div>
-                                <div className="w-7 right inline-block float-right">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                <div onClick={deleteGame} className="w-7 right inline-block float-right">
+                                    <svg  xmlns="http://www.w3.org/2000/svg"
                                          viewBox="0 0 448 512" fill="red">
                                         <path
                                             d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/>
