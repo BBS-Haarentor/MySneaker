ee91ad6862f7395153b954558651099d239645df
Optischa <fabian.evers2602@gmail.com>
2023-04-13 09:21:42 +0200

    Update machine container capacity

--- mysneaker/src/components/Container/Machine.js
+++ mysneaker/src/components/Container/Machine.js
@@ -15,7 +15,6 @@ export const MachineContainer = ({
 
     useEffect(() => {
         if (!workers_name) return;
-        console.log(workers_name)
         handleChange({
             target: {
                 name: workers_name,
@@ -26,10 +25,28 @@ export const MachineContainer = ({
 
     if (tempData === undefined || !tempData) return (<></>)
 
+    const auslastung = (planned_production / kapazität) * 100;
+
     return (
         <>
             <div
                 className={"p-4 dark:bg-[#1f2733] relative dark:text-white shadow-lg rounded-3xl m-2 bg-white max-[1200px]:w-[45%] max-[612px]:w-[96%] min-[1200px]:w-1/3 h-[500px]" + (hasError ? " border-red-300 border-2" : "")}>
+                <div
+                    className="h-3 absolute border-2 border-[#4ecfc3] rotate-[270deg] box-content flex rounded-full overflow-hidden font-[0.75rem] w-[250px] right-[-50px] top-[250px]">
+                    <div
+                        style={{width: (auslastung: 100) + "%", transition: "all 0.2s ease"}}
+                        className={"flex relative flex-col justify-center box-content text-center bg-green-500 rounded-full bg-gradient-to-r" + (auslastung <= 50 ? " from-green-300 to-green-500" : auslastung <= 90 ? " from-green-300 to-orange-500" : " from-green-500 via-orange-600 to-red-600")}
+                        role="progressbar">
+                    </div>
+                </div>
+                <div className="bg-[#4ecfc3] rounded-full w-10 h-10 absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         className={"w-8 h-8 my-1 fill-white"}
+                         viewBox="0 0 320 512">
+                        <path
+                            d="M48.1 240c-.1 2.7-.1 5.3-.1 8v16c0 2.7 0 5.3 .1 8H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H60.3C89.9 419.9 170 480 264 480h24c17.7 0 32-14.3 32-32s-14.3-32-32-32H264c-57.9 0-108.2-32.4-133.9-80H256c17.7 0 32-14.3 32-32s-14.3-32-32-32H112.2c-.1-2.6-.2-5.3-.2-8V248c0-2.7 .1-5.4 .2-8H256c17.7 0 32-14.3 32-32s-14.3-32-32-32H130.1c25.7-47.6 76-80 133.9-80h24c17.7 0 32-14.3 32-32s-14.3-32-32-32H264C170 32 89.9 92.1 60.3 176H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H48.1z"/>
+                    </svg>
+                </div>
                 <h1 className={"text-center text-2xl font-bold text-[#4ecfc3]"}>{name}</h1>
                 <h2 className={"mt-7 mb-5 text-center text-xl font-bold" + (tempData.max_production >= tempData.overall_production ? "" : " text-red-300")}>Geplante
                     Produktion</h2>
@@ -108,14 +125,14 @@ export const Maschine = ({
                 <h1 className="my-5 text-3xl w-full text-center text-[#4fd1c5] font-bold">Produktion</h1>
 
                 <div
-                    className="flex w-full min-[1200px]:flex-nowrap max-[1200px]:flex-wrap flex-row justify-center items-center overflow-x-hidden">
+                    className="flex w-full min-[1200px]:flex-nowrap max-[1200px]:flex-wrap flex-row justify-center overflow-x-hidden">
                     {data.stock.machine_1_space !== 0 ?
                         <MachineContainer name={machines[0].name} kapazität={machines[0].kapazität}
                                           handleChange={handleChange} workers_name={"planned_workers_1"}
                                           planned_production={cycle.planned_production_1}
                                           planned_production_name={"planned_production_1"} tempData={tempData}
                                           hasError={!(cycle.planned_workers_1 === Math.ceil((isNaN(cycle.planned_production_1) ? 0 : cycle.planned_production_1) / 20) && tempData.max_production >= tempData.overall_production && cycle.employees_count >= tempData.overall_workers)}
-                                          workersAvailable={cycle.planned_workers_1 === parseInt(Math.ceil(cycle.planned_production_1 / 20)) && cycle.employees_count >= tempData.overall_workers}
+                                          workersAvailable={cycle.planned_workers_1 === parseInt(Math.ceil((isNaN(cycle.planned_production_1) ? 0 : cycle.planned_production_1) / 20)) && cycle.employees_count >= tempData.overall_workers}
                                           overallCost={formatter.format(data.scenario.machine_maintainance_cost1 + data.scenario.production_cost_per_sneaker1 * (isNaN(cycle.planned_production_1) ? 0 : cycle.planned_production_1))}/>
                         : <BuyNewMachine ProductionRef={ProductionRef} onBuy={null}/>}
 
@@ -124,7 +141,7 @@ export const Maschine = ({
                                           handleChange={handleChange} workers_name={"planned_workers_2"}
                                           planned_production={cycle.planned_production_2}
                                           planned_production_name={"planned_production_2"} tempData={tempData}
-                                          hasError={!(cycle.planned_workers_2 === Math.ceil(cycle.planned_production_2 / 20) && tempData.max_production >= tempData.overall_production && cycle.employees_count >= tempData.overall_workers)}
+                                          hasError={!(cycle.planned_workers_2 === Math.ceil((isNaN(cycle.planned_production_2) ? 0 : cycle.planned_production_2) / 20) && tempData.max_production >= tempData.overall_production && cycle.employees_count >= tempData.overall_workers)}
                                           workersAvailable={cycle.planned_workers_2 === parseInt(Math.ceil((isNaN(cycle.planned_production_2) ? 0 : cycle.planned_production_2) / 20)) && cycle.employees_count >= tempData.overall_workers}
                                           overallCost={formatter.format(machines[1].costpp + machines[1].fertigungskostenpp * (isNaN(cycle.planned_production_2) ? 0 : cycle.planned_production_2))}/>
                         : cycle.buy_new_machine !== 0 ?
@@ -133,12 +150,12 @@ export const Maschine = ({
                                 <NoMachineBuy ProductionRef={ProductionRef}/>}
 
                     {data.stock.machine_3_space !== 0 ?
-                        <MachineContainer name={machines[1].name} kapazität={machines[2].kapazität}
+                        <MachineContainer name={machines[2].name} kapazität={machines[2].kapazität}
                                           handleChange={handleChange} workers_name={"planned_workers_3"}
                                           planned_production={cycle.planned_production_3}
                                           planned_production_name={"planned_production_3"} tempData={tempData}
                                           hasError={!(cycle.planned_workers_3 === Math.ceil((isNaN(cycle.planned_production_3) ? 0 : cycle.planned_production_3) / 20) && tempData.max_production >= tempData.overall_production && cycle.employees_count >= tempData.overall_workers)}
-                                          workersAvailable={cycle.planned_workers_3 === parseInt(Math.ceil(cycle.planned_production_3 / 20)) && cycle.employees_count >= tempData.overall_workers}
+                                          workersAvailable={cycle.planned_workers_3 === parseInt(Math.ceil((isNaN(cycle.planned_production_3) ? 0 : cycle.planned_production_3) / 20)) && cycle.employees_count >= tempData.overall_workers}
                                           overallCost={formatter.format(machines[2].costpp + machines[2].fertigungskostenpp * (isNaN(cycle.planned_production_3) ? 0 : cycle.planned_production_3))}/>
                         : data.stock.machine_2_space === 0 ?
                             <></> : cycle.buy_new_machine !== 0 ?
